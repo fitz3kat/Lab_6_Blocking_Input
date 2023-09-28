356d5633360f4a469fef50d5bdc23cc4506c71dd
fitz3 <fitz3kat@gmail.com>
2023-09-28 12:52:00 -0400

    first task completed

---
+++ src/CtoFConverter.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CtoFConverter {
+    public static void main(String[] args) {
+        double cTemp = 0;
+        double fTemp = 0;
+        boolean done = false;
+        String trash = "";
+
+        Scanner in = new Scanner(System.in);
+        do {
+            System.out.print("Enter a temperature in celsius: ");
+            if (in.hasNextDouble())
+            {
+                cTemp = in.nextDouble();
+                in.nextLine();
+                fTemp = (cTemp * 9/5) + 32;
+                System.out.println(cTemp + " in fahrenheit is " + fTemp);
+
+                done = true;
+
+            }
+
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("You told me " + trash);
+                System.out.println("Please try again and enter a numerical temperature.");
+            }
+
+
+        } while (!done);
+    }
+}
